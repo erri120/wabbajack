0aa950712b3fe9770f5ab3dab83ea1150dfdd1b3
erri120 <erri120@protonmail.com>
2020-11-19 16:56:31 +0100

    Create ReactiveMetroWindow
    Simple class that combines ReactiveWindow with MetroWindow.

---
+++ Wabbajack/Windows/ReactiveMetroWindow.cs
@@ -0,0 +1,59 @@
+﻿// /*
+//     Copyright (C) 2020  erri120
+// 
+//     This program is free software: you can redistribute it and/or modify
+//     it under the terms of the GNU General Public License as published by
+//     the Free Software Foundation, either version 3 of the License, or
+//     (at your option) any later version.
+// 
+//     This program is distributed in the hope that it will be useful,
+//     but WITHOUT ANY WARRANTY; without even the implied warranty of
+//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//     GNU General Public License for more details.
+// 
+//     You should have received a copy of the GNU General Public License
+//     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// */
+
+#nullable enable
+
+using System.Windows;
+using MahApps.Metro.Controls;
+using ReactiveUI;
+
+namespace Wabbajack.Windows
+{
+    public class ReactiveMetroWindow<TViewModel> : 
+        MetroWindow, IViewFor<TViewModel>
+        where TViewModel : class
+    {
+        /// <summary>
+        /// The view model dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(
+                "ViewModel",
+                typeof(TViewModel),
+                typeof(ReactiveWindow<TViewModel>),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the binding root view model.
+        /// </summary>
+        public TViewModel? BindingRoot => ViewModel;
+
+        /// <inheritdoc/>
+        public TViewModel? ViewModel
+        {
+            get => (TViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        /// <inheritdoc/>
+        object? IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (TViewModel?)value;
+        }
+    }
+}
