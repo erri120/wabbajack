1f07a13c66471d8f7371dbcba2859aaa88ed3c4c
erri120 <erri120@protonmail.com>
2020-11-21 15:16:08 +0100

    Create ReactiveMetroWindow
    A MetroWindow that is reactive.

---
+++ Wabbajack/Windows/ReactiveMetroWindow.cs
@@ -0,0 +1,47 @@
+﻿using System.Windows;
+using MahApps.Metro.Controls;
+using ReactiveUI;
+using Wabbajack.Lib;
+
+#nullable enable
+
+namespace Wabbajack.Windows
+{
+    /// <summary>
+    /// A <see cref="MetroWindow"/> that is reactive.
+    /// </summary>
+    /// <typeparam name="TViewModel"></typeparam>
+    public class ReactiveMetroWindow<TViewModel> :
+        MetroWindow, IViewFor<TViewModel>
+        where TViewModel : ViewModel
+    {
+        /// <summary>
+        /// The view model dependency property.
+        /// </summary>
+        public static readonly DependencyProperty ViewModelProperty =
+            DependencyProperty.Register(
+                "ViewModel",
+                typeof(TViewModel),
+                typeof(ReactiveWindow<TViewModel>),
+                new PropertyMetadata(null));
+
+        /// <summary>
+        /// Gets the binding root view model.
+        /// </summary>
+        public TViewModel? BindingRoot => ViewModel;
+
+        /// <inheritdoc/>
+        public TViewModel? ViewModel
+        {
+            get => (TViewModel)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+
+        /// <inheritdoc/>
+        object? IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (TViewModel?)value;
+        }
+    }
+}
